fb691fca8d6c84ee3ed66eace7e66722696aa113
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2016-03-29 14:50:58 +0200

    Added ng-view section

---
+++ app/js/controller/ViewStudentsCtrl.js
@@ -0,0 +1,13 @@
+/**
+ * @ngdoc controller
+ * @name myApp:ViewStudentsCtrl
+ *
+ * @description
+ *
+ *
+ * @requires $scope
+ * */
+angular.module('myApp')
+    .controller('ViewStudentsCtrl', function($scope){
+        $scope.message = "This page will be used to display all the students";
+    });
